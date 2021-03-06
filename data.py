fc4602a9b1545c7cce98c54c56e86f1e4e23d8aa
drumilT <drumiltrivedi@gmail.com>
2021-03-06 06:17:26 +0530

    data.py done but todos remain

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ from spacy.lang.en import English
 import json
 import gensim
 import xml.etree.ElementTree as ET 
+import os
 #from nltk.corpus import brown
 
 nlp = English()
@@ -27,7 +28,7 @@ def parse_XML(file):
                 y = g.attrib["senseid"]
             elif g.tag == "context":
                 sent = g.text
-                prep_mask = 0 ##to make
+                prep_mask = 0 ##to make #index of word
         mask_sent.append([sent,prep_mask,y])
         
     return root.attrib["item"],mask_sent
@@ -71,77 +72,76 @@ def get_glove():
 				embedding_dict[word] = vector
 
 
-
-def prep_feats(file_path):
+def prep_feats(file_path,window_size):    
 	get_glove() 
-# embedding_dict = get_word2vec()
 	ready_data_single = []
 	ready_data   = []
 	tagged_data  = []
-	corpus_sentences = parse_XML(file_path)
+	prep,corpus_sentences = parse_XML(file_path)
 	print(len(corpus_sentences))
 	oov_count = 0
 	for sent in corpus_sentences:
 		processed_sent = []
-		sent_labels    = []
-		for w_t in sent:
-			sent_labels.append(w_t[1])
-			word_data = dict({})
-			word_data["word"]    = w_t[0] .lower()
-			word_data["capital"] = any(x.upper for x in w_t[0])  
-				# if w_t[0].lower() not in embedding_dict.keys():
-			if w_t[0].lower() not in embedding_dict.keys():
-				tokens = [ str(tok) for tok in nlp(w_t[0].lower())]
-				stemmed = stemmer.stem(w_t[0])
-				tk_count = np.sum([1 for k in tokens if k in embedding_dict.keys() ])
-				vector = np.zeros(glove_dim)
-				if tk_count != 0:
-					vector = np.sum( [embedding_dict[tok] for tok in tokens if tok in embedding_dict.keys()], axis=0)/len(tokens)
-				elif stemmed in  embedding_dict.keys():
-					vector = embedding_dict[stemmed]
-					if np.all( vector) ==0:
-						oov_count += 1
-			else:
-				vector = embedding_dict[w_t[0].lower()]
-			pref,suff = get_prefix_suffix(w_t[0])
-			# word_data["word"] = w_t[0].lower()
-			word_data["pos_tag"] = w_t[2]
-			word_data["prefix"] = pref 
-			word_data["suffix"] = suff
-			processed_sent.append(word_data)
-		tagged_data.append(sent_labels)
-		ready_data_single.append(processed_sent)
+		sent_labels    = [sent[-1]]
 
-	for sent in ready_data_single:
-		new_sent = []
-		padded_sent = sent + [{"word":"PAD", "pos_tag": "PAD"},{"word":"PAD","pos_tag": "PAD"}]
-		prev_vect = ["POS","POS"]
-		# print(padded_sent)
-		next_vect  = [padded_sent[1]['word'],padded_sent[2]['word']]
-		prev_pos = ["PAD","PAD"]
-		next_pos = [padded_sent[1]["pos_tag"],padded_sent[2]["pos_tag"]]
-		for idx in range(len(padded_sent)-2):
-			curr_word = padded_sent[idx]
-			curr_word["prev_vector_0"] = prev_vect[0] 
-			curr_word["prev_vector_1"] = prev_vect[1]
-			curr_word["next_vector_1"] = next_vect[1] 
-			curr_word["next_vector_0"] = next_vect[0]
-			curr_word["prev_pos_0"] = prev_pos[0]
-			curr_word["prev_pos_1"] = prev_pos[1]
-			curr_word["next_pos_0"] = next_pos[0]
-			curr_word["next_pos_1"] = next_pos[1]
-			prev_vect = [prev_vect[1],curr_word["word"]]
-			next_vect  = [next_vect[1],padded_sent[idx+2]["word"]]
+        for w in sent[0]:
+            word_data = dict({})
+            word_data["word"]    = w.lower()
+            word_data["capital"] = any(x.upper for x in w)  
+                # if w_t[0].lower() not in embedding_dict.keys():
+            if w.lower() not in embedding_dict.keys():
+                tokens = [ str(tok) for tok in nlp(w.lower())]
+                stemmed = stemmer.stem(w)
+                tk_count = np.sum([1 for k in tokens if k in embedding_dict.keys() ])
+                vector = np.zeros(glove_dim)
+                if tk_count != 0:
+                    vector = np.sum( [embedding_dict[tok] for tok in tokens if tok in embedding_dict.keys()], axis=0)/len(tokens)
+                elif stemmed in  embedding_dict.keys():
+                    vector = embedding_dict[stemmed]
+                    if np.all( vector) ==0:
+                        oov_count += 1
+            else:
+                vector = embedding_dict[w.lower()]
+            word_data["vector"] = vector
+            processed_sent.append(word_data)
+        tagged_data.append(sent_labels)
+        ready_data_single.append([processed_sent,sent[1]])
 
-			prev_pos = [prev_pos[1],curr_word["pos_tag"]]
-			next_pos  = [next_pos[1],padded_sent[idx+2]["pos_tag"]]
-			new_sent.append(["{}={}".format(x,curr_word[x]) for x in curr_word.keys()])
+	for sent,indx in ready_data_single:
+		new_sent = dict({})
+		vl = np.stack(sent[max(0,indx-window_size):indx],axis = -1) 
+        vr = np.stack(sent[indx:min(len(sent),indx+window_size)],axis = -1) 
+		print(vl.shape,vr.shape())
+        vi = np.concatenate((vl,vr),axis=1)
+        print(vi.shape)
+        new_sent["vl"]= np.mean(vl,axis=1)
+        new_sent["vr"]= np.mean(vr,axis=1)
+        new_sent["vi"]= 0 #todo add code to get primary pca
+
 		ready_data.append(new_sent)
 	corpus_sentences = ready_data
 	corpus_labels    = tagged_data
 	print("OOV count"+ str(oov_count))
-	return corpus_sentences, corpus_labels
+	return prep,corpus_sentences, corpus_labels
+
+def gen_train_test(window_size):
+    fpath = "data_assn1/{}/Source/"
+    train = dict({})
+    test = dict({})
+    for typr in ["Train","Test"]
+        for filename in os.listdir(fpath.format(typr)):
+            print(filename)
+            prep,sents,labels = prep_feats(filename,window_size)
+            print(prep)
+            if typr=="Train":
+                train[prep] = { "sents":sents , "labels":labels}
+            else:
+                test[prep] = { "sents":sents , "labels":labels}
+    return train,test
+
+
 
 if __name__ == '__main__':
-	ts,tl = prep_feats('../assignment2dataset/train.txt')
-	print(ts[0],tl[0])
+	p,ts,tl = prep_feats('data_assn1/Train/Source/pp-about.sents.trng.xml',4)
+	assert(p=="about")
+    print(ts[0]['vl'].shape,tl[0])
